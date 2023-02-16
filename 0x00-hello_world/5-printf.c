04b569352589ebccdf7cf026e92a96b0c6eb838e
millywanderi <milliwanderi5@gmail.com>
2023-02-16 03:00:25 -0800

    0x00-hello_world task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - my starting point
+ * Return: the return value should be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
