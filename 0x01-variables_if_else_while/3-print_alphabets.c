143a3c2298619edc31a2683362a7edd4b9fa092a
millywanderi <milliwanderi5@gmail.com>
2023-02-17 12:50:43 +0000

    0x01-variables_if_else_while task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	char alpha;
+	char ALPHA;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (ALPHA = 'A'; ALPHA <= 'Z'; ALPHA++)
+	{
+		putchar(ALPHA);
+	}
+	putchar('\n');
+	return (0);
+}
