3c9d36c8e4146ed97427ebcb05ddac2498855919
millywanderi <milliwanderi5@gmail.com>
2023-03-08 01:42:56 -0800

    return 1 for prime numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,18 +10,6 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if (n == 0)
-	{
-		return (0);
-	}
-	else if (n == 1)
-	{
-		return (1);
-	}
 	return (_sqrt(n, 1));
 }
 
