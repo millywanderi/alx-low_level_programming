dbc032dc246b2999b81a55a13615827f5f21b784
millywanderi <milliwanderi5@gmail.com>
2023-03-10 23:23:02 -0800

    program should print the result of the multiplication

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,20 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = 2;
-	int num2 = 3;
-	int mul = num1 * num2;
+	int m;
+	int num1;
+	int num2;
+	int mul;
 
-	if (argc == 3)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		printf("%d\n", mul);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	else
+	{
+		for (m = 0; m < argc; m++)
+		{
+			num1 = atoi(argv[1]);
+			num2 = atoi(argv[2]);
+			mul = num1 * num2;
+		}
+	}
+	printf("%d\n", mul);
 	return (0);
 }
