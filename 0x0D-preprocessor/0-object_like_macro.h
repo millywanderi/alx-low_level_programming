7638b8946a5f425fda503b485d47a01bdb492f4e
millywanderi <milliwanderi5@gmail.com>
2023-03-20 03:07:04 -0700

    a header file that defines a macro named SIZE as an abbreviation for the token 1024

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * OBJECT_LIKE_MACRO_H - header file defining SIZE holding token 1024
+  */
+
+#ifdef OBJECT_LIKE_MACRO_H
+
+#define OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif
