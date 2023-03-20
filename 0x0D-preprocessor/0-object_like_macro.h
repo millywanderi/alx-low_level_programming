fdf46dd9a7daf188bc51d1445d79e868674b3ec0
millywanderi <milliwanderi5@gmail.com>
2023-03-20 09:39:23 -0700

    updated file

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,5 @@
-#include <stdio.h>
-
-/**
-  * OBJECT_LIKE_MACRO_H - header file defining SIZE holding token 1024
-  */
-
-#ifdef OBJECT_LIKE_MACRO_H
-
-#define OBJECT_LIKE_MACRO_H
+#ifdef SIZE_H
+#define SIZE_H
 
 #define SIZE 1024
 
