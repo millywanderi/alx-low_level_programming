906b61536c2d667a5e4fe619b9a064d0e5dbbc9a
millywanderi <milliwanderi5@gmail.com>
2023-03-20 10:19:42 -0700

    a header file that defines a macro named PI as an abbreviation for the token 3.14159265359

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,11 +1,5 @@
-#include <stdio.h>
-
-/**
-  * PI_H - a header file that defines a macro named PI as an
-  * abbreviation for the token 3.14159265359.
-  */
-
 #ifndef PI_H
+#define PI_H
 
 #define PI 3.14159265359
 
