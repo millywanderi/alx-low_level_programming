f73dfdb77568ed7dc2e5e6d89fa68be85724afe8
millywanderi <wanderinyambura5@gmail.com>
2023-09-12 02:47:10 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,7 +9,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void arr_print(int *array, size_t start, size_t end);
+int recursive_helper(int *array, size_t left, size_t right, int value);
 int jump_search(int *array, size_t size, int value);
 int subrecurse_help(int *array, size_t size, size_t end, size_t idx, int val);
 int recurse_helper(int *array, size_t size, size_t step, size_t idx, int val);
