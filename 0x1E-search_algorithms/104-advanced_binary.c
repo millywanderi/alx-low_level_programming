0dddbd17ca18d55f033f90bd325b443b416900bd
millywanderi <wanderinyambura5@gmail.com>
2023-09-13 10:05:24 -0700

    searches for a value in a sorted array of integers

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,5 +1,8 @@
 #include "search_algos.h"
 
+void print_array(int *array, size_t lo, size_t hi);
+int help_binary(int *array, int value, size_t lo, size_t hi);
+
 /**
  * advanced_binary - searches for a value in a sorted array of integers.
  * @array: pointer to the first element of the array to search in
@@ -9,53 +12,54 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t mid;
-	int result;
-
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
+	return (help_binary(array, value, 0, size - 1));
 
-	if (size == 1)
-	{
-		if (*array == value)
-			return (0);
-		return (-1);
-	}
-	printf("Searching in array: ");
-	print_array(array, size);
-
-	mid = (size - 1) / 2;
-
-	if (array[mid] == value)
-	{
-		if (mid > 0 && array[mid - 1] == value)
-			return (advanced_binary(array, mid + 1, value));
-		return (mid);
-	}
-	else if (array[mid] < value)
-	{
-		result = advanced_binary(&array[mid + 1], size - mid, value);
-		if (result == 1)
-			return (-1);
-		return (mid + 1 + result);
-	}
-	return (advanced_binary(array, mid + 1, value));
 }
 
 /**
  * print_array - prints array
  * @array: pointer to the first element of the array to print
- * @size: Number of elements in array.
+ * @lo: lower bound
+ * @hi: high bound
  */
-void print_array(int *array, size_t size)
+void print_array(int *array, size_t lo, size_t hi)
 {
 	size_t m;
 
-	for (m = 0; m < size; m++)
+	printf("Searching in array: ");
+	for (m = lo; m <= hi; m++)
 	{
 		printf("%d", array[m]);
-		if (m != size - 1)
+		if (m < hi)
 			printf(", ");
 	}
 	printf("\n");
+}
+
+/**
+ * help_binary - searches for a value in an array
+ * @array: search array
+ * @value: search value
+ * @lo: low bound
+ * @hi: high bound
+ *
+ * Return: index for the found value, otherwise -1
+ */
+int help_binary(int *array, int value, size_t lo, size_t hi)
+{
+	size_t mid;
+
+	print_array(array, lo, hi);
+	if (hi == lo && array[lo] != value)
+		return (-1);
+	mid = ((hi - lo) / 2) + lo;
+	if (array[mid] == value)
+		return (mid);
+	if (array[mid] < value)
+		return (help_binary(array, value, mid + 1, hi));
+	if (array[mid] > value)
+		return (help_binary(array, value, lo, mid - 1));
+	return (-1);
 }
